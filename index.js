d88d151a868e8c9ab54863f06b1c954932faef8b
Nirand Pisutha-Arnond <nnnpooh@gmail.com>
2023-07-11 14:12:49 +0700

    Init

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const port = 3001;
+
+var corsOptions = {
+  origin: "*",
+  optionsSuccessStatus: 200,
+};
+
+app.get("/", cors(corsOptions), (req, res) => {
+  const now = new Date();
+  res.send({
+    year: now.getFullYear(),
+    month: now.getMonth() + 1,
+    date: now.getDate(),
+    hour: now.getHours(),
+    minute: now.getMinutes(),
+    second: now.getSeconds(),
+    timestamp: now.getTime(),
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
